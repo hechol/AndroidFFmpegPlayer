22c456c2328defb37da5b03dde1a6a1d1a30e564
2iGi94A5IC <maner9@gmail.com>
2018-09-09 14:47:16 +0900

    일단 queue 메서드만 추가

--- app/src/main/cpp/BasicPlayer.c
+++ app/src/main/cpp/BasicPlayer.c
@@ -19,6 +19,8 @@ int audioStream;
 SwrContext *swr;
 AVFrame *audioFrame;
 
+AVPacket flush_pkt;
+
 // engine interfaces
 static SLObjectItf engineObject = NULL;
 static SLEngineItf engineEngine;
@@ -62,14 +64,115 @@ uint8_t *gVideoBuffer = NULL;
 uint8_t *audioOutputBuffer = NULL;
 int audio_data_size = 0;
 
-
-
 VideoState      *is;
 
+typedef struct PacketQueue {
+    AVPacketList *first_pkt, *last_pkt;
+    int nb_packets;
+    int size;
+    int abort_request;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *cond;
+} PacketQueue;
+
+/* packet queue handling */
+void packet_queue_init(PacketQueue *q)
+{
+    memset(q, 0, sizeof(PacketQueue));
+    pthread_mutex_init(q->mutex, NULL);
+    pthread_cond_init(q->cond, NULL);
+}
+
+void packet_queue_flush(PacketQueue *q)
+{
+    AVPacketList *pkt, *pkt1;
+
+    pthread_mutex_lock(q->mutex);
+    for(pkt = q->first_pkt; pkt != NULL; pkt = pkt1) {
+        pkt1 = pkt->next;
+        av_free_packet(&pkt->pkt);
+        av_freep(&pkt);
+    }
+    q->last_pkt = NULL;
+    q->first_pkt = NULL;
+    q->nb_packets = 0;
+    q->size = 0;
+    pthread_mutex_unlock(q->mutex);
+}
+
+int packet_queue_put(PacketQueue *q, AVPacket *pkt)
+{
+    AVPacketList *pkt1;
+
+    /* duplicate the packet */
+    if (pkt!=&flush_pkt && av_dup_packet(pkt) < 0)
+        return -1;
+
+    pkt1 = av_malloc(sizeof(AVPacketList));
+    if (!pkt1)
+        return -1;
+    pkt1->pkt = *pkt;
+    pkt1->next = NULL;
+
+    pthread_mutex_lock(q->mutex);
+
+    if (!q->last_pkt)
+
+        q->first_pkt = pkt1;
+    else
+        q->last_pkt->next = pkt1;
+    q->last_pkt = pkt1;
+    q->nb_packets++;
+    q->size += pkt1->pkt.size + sizeof(*pkt1);
+    /* XXX: should duplicate packet data in DV case */
+    pthread_cond_signal(q->cond);
+
+    pthread_mutex_unlock(q->mutex);
+    return 0;
+}
+
+/* return < 0 if aborted, 0 if no packet and > 0 if packet.  */
+int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block)
+{
+    AVPacketList *pkt1;
+    int ret;
+
+    pthread_mutex_lock(q->mutex);
+
+    for(;;) {
+        if (q->abort_request) {
+            ret = -1;
+            break;
+        }
+
+        pkt1 = q->first_pkt;
+        if (pkt1) {
+            q->first_pkt = pkt1->next;
+            if (!q->first_pkt)
+                q->last_pkt = NULL;
+            q->nb_packets--;
+            q->size -= pkt1->pkt.size + sizeof(*pkt1);
+            *pkt = pkt1->pkt;
+            av_free(pkt1);
+            ret = 1;
+            break;
+        } else if (!block) {
+            ret = 0;
+            break;
+        } else {
+            pthread_cond_wait(q->cond, q->mutex);
+        }
+    }
+    pthread_mutex_unlock(q->mutex);
+    return ret;
+}
 
 void createEngine()
 {
     is = av_mallocz(sizeof(VideoState));
+
+    av_init_packet(&flush_pkt);
+    flush_pkt.data= "FLUSH";
 
     SLresult result;
 
@@ -104,6 +207,8 @@ void createEngine()
 
 int openMovie(ANativeWindow* nativeWindow, const char filePath[])
 {
+
+
     createEngine();
 
     int video_index, audio_index;
@@ -137,6 +242,8 @@ int openMovie(ANativeWindow* nativeWindow, const char filePath[])
     if (video_index >= 0) {
         stream_component_open(is, video_index, nativeWindow);
     }
+
+
 
     for(;;){
         decodeFrame(nativeWindow);
--- app/src/main/cpp/BasicPlayer.h
+++ app/src/main/cpp/BasicPlayer.h
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <android/bitmap.h>
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
